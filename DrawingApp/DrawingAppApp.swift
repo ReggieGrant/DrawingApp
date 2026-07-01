c446eadd3d16dcb18c568e8e5045b83ead5d142f
ReggieGrant <grantreggie09@gmail.com>
2026-06-30 18:32:45 -0700

    Initial Commit

---
+++ DrawingApp/DrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAppApp.swift
+//  DrawingApp
+//
+//  Created by Reginald Grant on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrawingAppTests/DrawingAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  DrawingAppTests.swift
+//  DrawingAppTests
+//
+//  Created by Reginald Grant on 6/30/26.
+//
+
+import Testing
+@testable import DrawingApp
+
+struct DrawingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
